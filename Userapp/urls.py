8ccfdb35d329a1f581036b720d469811688090a4
Sharifjon <mominovsharif12@gmail.com>
2023-10-20 18:02:08 +0500

    Biz UDDALADIK

--- Userapp/urls.py
+++ Userapp/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
-from .views import DescriptionMoney,KirimMoney,ChiqimMoney,KirimChiqimfarqi
+from .views import DescriptionMoney, KirimMoney, ChiqimMoney, KirimChiqimfarqi, Top3, TopUsers3
 
 urlpatterns = [
     path('hamma_pul/', DescriptionMoney.as_view()),
     path('kirim/', KirimMoney.as_view()),
     path('chiqim/', ChiqimMoney.as_view()),
-    path('farq/',KirimChiqimfarqi.as_view())
+    path('farq/', KirimChiqimfarqi.as_view()),
+    path('top3/', Top3.as_view()),
+    path('topuser/', TopUsers3.as_view()),
 ]
--- Userapp/views.py
+++ Userapp/views.py
@@ -80,3 +80,50 @@ class KirimChiqimfarqi(APIView):
         return Response({"Xarajatlar natijasi : ": summa,
                          "Kirim": kirim,
                          "Chiqim:": chqim})
+
+
+# filter top3 users in get APIView
+class Top3(APIView):
+    serializer_class = Money_monthly_serializer
+    queryset = UserRegistration.objects.all()
+
+    def get(self, request):
+        money = MoneyManagment.objects.all().order_by('-narx')[:3]
+        serializer_pulcha = Money_monthly_serializer_send(money, many=True)
+        return Response(serializer_pulcha.data)
+
+
+class TopUsers3(APIView):
+    serializer_class = Money_monthly_serializer
+    queryset = UserRegistration.objects.all()
+
+    def get(self, request):
+        MINI_DATABASE_DICT = {}
+        # all users username in list
+        users = UserRegistration.objects.all()
+        for i in users:
+            print(i.username)
+            user = UserRegistration.objects.get(username=i.username)
+            money_chiqim = MoneyManagment.objects.all().filter(type_payment="Chiqim", user=user)
+            serializer_pulcha_chiqim = Money_monthly_serializer_send(money_chiqim, many=True)
+            chqim = 0
+            for g in serializer_pulcha_chiqim.data:
+                chqim += g['narx']
+
+            money_kirim = MoneyManagment.objects.all().filter(type_payment="Kirim", user=user)
+            serializer_pulcha_kirim = Money_monthly_serializer_send(money_kirim, many=True)
+            kirim = 0
+
+            for d in serializer_pulcha_kirim.data:
+                kirim += d['narx']
+            MINI_DATABASE_DICT[kirim - chqim] = i.username
+        # sort by money DATABASE_DICT_MONEY
+        pullar = list(MINI_DATABASE_DICT.keys())
+        pullar.sort(reverse=True)
+        txt = ""
+        iter = 0
+        for j in pullar:
+            iter += 1
+            if iter<=3:
+                txt+= f"{MINI_DATABASE_DICT[j]} : {j} , "
+        return Response(txt)
